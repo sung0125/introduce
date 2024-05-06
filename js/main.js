404eefcdb443706dc7c9ebe72ddd0b280e8ea97a
전성배 <sungbae0125@gmail.com>
2024-05-07 04:12:26 +0900

    배경 스크롤이벤트 전까지

--- js/main.js
+++ js/main.js
@@ -15,3 +15,55 @@ function typing() {
 setInterval(typing, 200);
 
 //스크롤 애니메이션
+
+// Favorite Slide
+const slides = document.querySelector(".slides");
+const slide = document.querySelectorAll(".slides li");
+const slideCount = slide.length;
+
+let currentIdx = 0;
+
+let slideWidth = 300;
+let slideMargin = 30;
+
+slides.style.width = (slideWidth + slideMargin) * slideCount - slideMargin + "px";
+
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+function moveSlide(num) {
+    // slide 가 움직일때 slide index가 1이면 -330px 2면 -660px
+    slides.style.left = -num * 330 + "px";
+    currentIdx = num;
+}
+
+nextBtn.addEventListener("click", function () {
+    if (currentIdx < slideCount - 3) {
+        moveSlide(currentIdx + 1);
+    } else {
+        moveSlide(0);
+    }
+});
+
+prevBtn.addEventListener("click", function () {
+    if (currentIdx > 0) {
+        moveSlide(currentIdx - 1);
+    } else {
+        moveSlide(slideCount - 3);
+    }
+});
+
+let contactTilte = document.querySelector("#footer .footer-box");
+let contactLink = document.querySelector("#footer div");
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+
+    if (value > 1950) {
+        contactTilte.style.animation = "grow 0.8s linear";
+        contactLink.style.animation = "grow 0.8s linear";
+    } else {
+        contactTilte.style.animation = "grow-out 0.5s linear";
+        contactLink.style.animation = "grow-out 0.5s linear";
+    }
+});
